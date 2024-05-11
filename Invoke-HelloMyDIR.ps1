639b830890dfc26ac4ff0cdf08bd18861659e5e4
Loic Veirman <github@mssec.fr>
2024-05-11 16:17:58 +0200

    Update Invoke-HelloMyDIR.ps1

--- Invoke-HelloMyDIR.ps1
+++ Invoke-HelloMyDIR.ps1
@@ -106,9 +106,11 @@ if ($Prepare) {
     }
 
     # Say Hello
-    $ScriptTitle = @(' ',"$([Char]0x2554)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2557)" `
-                        ,"$([Char]2551)Hello My DIR!$([Char]0x2551)","$([Char]0x2551)version 1.0.0$([Char]0x2551)","$([Char]0x2551)Lic. GNU GPL3$([Char]0x2551)" `
-                        ,"$([Char]0x255A)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x255D)" `
+    $ScriptTitle = @(' ',"$([Char]0x2554)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2557)" `
+                        ,"$([Char]0x2551) Hello My DIR! $([Char]0x2551)" `
+                        ,"$([Char]0x2551) version 1.0.0 $([Char]0x2551)" `
+                        ,"$([Char]0x2551) Lic. GNU GPL3 $([Char]0x2551)" `
+                        ,"$([Char]0x255A)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x2550)$([Char]0x255D)" `
                         ,' ')
     Write-TitleText -Text $ScriptTitle
 
