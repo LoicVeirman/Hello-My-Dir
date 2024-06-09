bfce838c69eaf992d801cfa604583280f4d5c9e0
Loic Veirman <github@mssec.fr>
2024-06-10 00:08:19 +0200

    Update Module-PC_3-2-0-1.psm1

--- Modules/Module-PC_3-2-0-1/Module-PC_3-2-0-1.psm1
+++ Modules/Module-PC_3-2-0-1/Module-PC_3-2-0-1.psm1
@@ -89,7 +89,7 @@ Function Resolve-S-DC-SubnetMissing {
     # Get IP PLAN ADDRESSES and add them to the default AD Site
     foreach ($DCIP in $DCIPs) {
         Try {
-            $IPplan = "$(([IPAddress] (([IPAddress] "$($DCIP.IPAddress)").Address -band ([IPAddress] (ConvertTo-IPv4MaskString $DCIP.PrefixLength)).Address)).IPAddressToString)\$($DCIP.PrefixLength)"
+            $IPplan = "$(([IPAddress] (([IPAddress] "$($DCIP.IPAddress)").Address -band ([IPAddress] (ConvertTo-IPv4MaskString $DCIP.PrefixLength)).Address)).IPAddressToString)/$($DCIP.PrefixLength)"
             $LogData += @(" ","Checking for IP Plan: $IPplan")
         }
         Catch {
@@ -116,8 +116,7 @@ Function Resolve-S-DC-SubnetMissing {
                 $LogData += "Subnet $IPplan has been added to '$DfltSite'"
             }
             Catch {
-                $LogData += @("Subnet $IPplan could not be added to '$DfltSite'!",' ')
-                $LogData += @('Error from stack:',$Error[1].ToString())
+                $LogData += @("Subnet $IPplan could not be added to '$DfltSite'!")
                 $FlagRes = "Error"
             }
         }
