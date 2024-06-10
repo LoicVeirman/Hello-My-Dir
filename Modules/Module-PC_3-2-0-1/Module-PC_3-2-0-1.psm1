ba95e714f93ce33349e76c62908131380786a71a
Loic Veirman <github@mssec.fr>
2024-06-10 18:07:27 +0200

    Update Module-PC_3-2-0-1.psm1

--- Modules/Module-PC_3-2-0-1/Module-PC_3-2-0-1.psm1
+++ Modules/Module-PC_3-2-0-1/Module-PC_3-2-0-1.psm1
@@ -527,7 +527,7 @@ Function Resolve-A-PreWin2000AuthenticatedUsers {
     Try {
         $members = Get-ADGroupMember "S-1-5-32-554"
         if ($members) {
-            remove-adGroupMember -Identity "S-1-5-32-554" -Members $members.SID
+            remove-adGroupMember -Identity "S-1-5-32-554" -Members $members.SamAccountName
             $LogData += "Group S-1-5-32-554 flushed."
         } 
         Else {
