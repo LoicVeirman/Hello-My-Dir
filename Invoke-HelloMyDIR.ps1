2e6784e75da8a3f9579481172ff25eb8a07f03e7
Loic Veirman <github@mssec.fr>
2024-06-12 22:01:24 +0200

    Update Invoke-HelloMyDIR.ps1

--- Invoke-HelloMyDIR.ps1
+++ Invoke-HelloMyDIR.ps1
@@ -371,9 +371,9 @@ Else {
                     if ($GPO.Linkage -eq "DC") {
                         $gpPath = "OU=Domain Controllers,$gpPath"
                     }
-                    [void](New-Gpo -Name $gpName -Comment $gpDesc -ErrorAction Stop)
+                    [void](New-Gpo -Name $gpo.Name -ErrorAction Stop)
                     [void](Import-GPO -BackupId $GPO.BackupId -TargetName $gpo.Name -Path .\Imports\$($GPO.Name) -ErrorAction Stop)
-                    [void](Set-GPLink -Name $gpo.Name -Target $gpPath -LinkEnabled $gpLink.Enabled -Enforced $gpLink.enforced -ErrorAction SilentlyContinue)
+                    [void](Set-GPLink -Name $gpo.Name -Target $gpPath -LinkEnabled $true -ErrorAction Stop)
                     Write-ToEventLog -EventType INFO -EventMsg "GPO $($GPO.Name): imported successfully."
                     $isSuccess++
                 }
